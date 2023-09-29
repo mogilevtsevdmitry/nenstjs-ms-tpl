5b3ec95897937da258ead1c9972cd9df37d98f99
Mogilevtsev Dmitry <webmogilevtsev@ya.ru>
2023-09-30 00:28:26 +0500

    фвв фьйз

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
+import { ConnectorsModule } from '@connectors';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
-    imports: [],
+    imports: [
+        ConnectorsModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: join(process.cwd(), '.env'),
+        }),
+    ],
 })
 export class AppModule {}
